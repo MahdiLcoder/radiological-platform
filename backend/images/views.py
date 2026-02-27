730dc7cd53218463e1c8d96cbdd7f081eb5a3ab6
Mahdi Labidi <labidimahdi95@gmail.com>
2026-02-27 21:18:05 +0100

    feat: Import APIException into images/views.py.

--- backend/images/views.py
+++ backend/images/views.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, APIException
 from .serializers import RadiologyImageSerializer
 from .models import RadiologyImage
 from accounts.permissions import IsRadiologist, IsAdmin, IsDoctor
