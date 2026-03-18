a8ae4fdc1c5672d303057b8e66acb0e7e2c5c2f5
Mahdi Labidi <labidimahdi95@gmail.com>
2026-03-18 05:18:34 +0100

    feat: add Reports page with UI, routing, and sidebar navigation.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,21 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadComponent: () => import('./pages/login/login').then(m => m.Login) },
-    {
-        path: 'dashboard',
-        loadComponent: () => import('./layout/layout').then(m => m.Layout),
-        children: [
-            { path: 'radiologist', loadComponent: () => import('./pages/radiologist/radiologist').then(m => m.Radiologist) }
-        ]
-    }
+  { path: '', loadComponent: () => import('./pages/login/login').then((m) => m.Login) },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./layout/layout').then((m) => m.Layout),
+    children: [
+      {
+        path: 'radiologist',
+        loadComponent: () => import('./pages/radiologist/radiologist').then((m) => m.Radiologist),
+      },
+      {
+        path: 'reports',
+        loadComponent: () => import('./pages/reports/reports').then((m) => m.Reports),
+      },
+      // { path: 'patients', loadComponent: () => import('./pages/patients/patients').then(m => m.Patients) },
+      // { path: 'analytics', loadComponent: () => import('./pages/analytics/analytics').then(m => m.Analytics) }
+    ],
+  },
 ];
