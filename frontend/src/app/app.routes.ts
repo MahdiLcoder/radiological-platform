083fba456c9a271d8012380c8f396478ce78cf67
Mahdi Labidi <labidimahdi95@gmail.com>
2026-03-18 04:41:03 +0100

    feat: Implement the radiologist dashboard page with its component, template, styles, and add a corresponding route.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,7 +6,7 @@ export const routes: Routes = [
         path: 'dashboard',
         loadComponent: () => import('./layout/layout').then(m => m.Layout),
         children: [
-
+            { path: 'radiologist', loadComponent: () => import('./pages/radiologist/radiologist').then(m => m.Radiologist) }
         ]
     }
 ];
---
+++ frontend/src/app/pages/radiologist/radiologist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radiologist',
+  imports: [],
+  templateUrl: './radiologist.html',
+  styleUrl: './radiologist.css',
+})
+export class Radiologist {
+
+}
