b597b8eff72593437b52d603776286e39e5c174c
Mahdi Labidi <labidimahdi95@gmail.com>
2026-03-18 09:34:37 +0100

    feat: add AI validation page with image viewer and analysis insights UI.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -22,6 +22,11 @@ export const routes: Routes = [
         path: 'admin',
         loadComponent: () => import('./pages/admin/admin').then((m) => m.Admin),
       },
+      {
+        path: 'aivalidation',
+        loadComponent: () => import('./pages/aivalidation/aivalidation').then((m) => m.Aivalidation),
+      },
+      
       // { path: 'patients', loadComponent: () => import('./pages/patients/patients').then(m => m.Patients) },
       // { path: 'analytics', loadComponent: () => import('./pages/analytics/analytics').then(m => m.Analytics) }
     ],
---
+++ frontend/src/app/pages/aivalidation/aivalidation.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aivalidation',
+  standalone: true,
+  imports: [],
+  templateUrl: './aivalidation.html',
+  styleUrl: './aivalidation.css',
+})
+export class Aivalidation {}
