389fc3820d235ecc5d1c1c0019680239ca13e75a
Mahdi Labidi <labidimahdi95@gmail.com>
2026-03-22 12:32:57 +0100

    feat: Implement role-based navigation and dynamically display sidebar content using user profile data.

--- frontend/src/app/components/sidebar/sidebar.ts
+++ frontend/src/app/components/sidebar/sidebar.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/authService';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,4 +10,13 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  private authService = inject(AuthService);
+
+  profileQuery = injectQuery(() => ({
+    queryKey: ['profile'],
+    queryFn: () => lastValueFrom(this.authService.getProfile()) as Promise<any>,
+    enabled: this.authService.isLoggedIn(),
+    retry: false,
+  }));
+}
--- frontend/src/app/layout/layout.ts
+++ frontend/src/app/layout/layout.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 import { Header } from '../components/header/header';
 import { Sidebar } from '../components/sidebar/sidebar';
+import { AuthService } from '../services/authService';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -9,6 +11,33 @@ import { Sidebar } from '../components/sidebar/sidebar';
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
-export class Layout {
+export class Layout implements OnInit {
+  private router = inject(Router);
+  private authService = inject(AuthService);
 
+  async ngOnInit() {
+    if (this.router.url === '/dashboard' ) {
+      try {
+        const profile = await lastValueFrom(this.authService.getProfile());
+        const role = profile.role?.toLowerCase();
+        
+        switch (role) {
+          case 'admin':
+            this.router.navigate(['/dashboard/admin']);
+            break;
+          case 'radiologist':
+            this.router.navigate(['/dashboard/radiologist']);
+            break;
+          case 'doctor':
+            this.router.navigate(['/dashboard/patients']);
+            break;
+          default:
+            this.router.navigate(['/']);
+            break;
+        }
+      } catch (error) {
+        console.error('Failed to fetch profile for redirection', error);
+      }
+    }
+  }
 }
